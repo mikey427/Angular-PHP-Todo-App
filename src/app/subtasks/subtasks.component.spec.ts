710595aaca057f70be9da7a416a386f95bea9813
Michael Baez <mrbaez427@gmail.com>
2021-08-20 18:23:25 -0400

    Set up tailwind, got basis for main page set up

---
+++ src/app/subtasks/subtasks.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SubtasksComponent } from './subtasks.component';
+
+describe('SubtasksComponent', () => {
+  let component: SubtasksComponent;
+  let fixture: ComponentFixture<SubtasksComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SubtasksComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SubtasksComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
